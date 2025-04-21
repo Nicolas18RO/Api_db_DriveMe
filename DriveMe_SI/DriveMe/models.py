d09b183513244b69b0d7b9660cf425019ab164b5
Nicolas18RO <nicorodrigueopayome@gmail.com>
2025-04-21 08:30:38 -0500

    Se agrega modelo Driver

--- DriveMe_SI/DriveMe/models.py
+++ DriveMe_SI/DriveMe/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Driver Model.
+class Driver(models.Model):
+    id_drive = models.AutoField(primary_key=True, editable=False)
+    document = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f'{self.name}{self.last_name}'
+    
+#Vehicle Model.
+class Vehicle(models.Model):
+    driver = models.OneToOneField(Driver, on_delete=models.CASCADE)
+    plate = models.CharField(max_length=6, unique=True)
+    
+
